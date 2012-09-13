b1d3edd2eb58ccf3405361e8a69316bc939687f9
Eric Sizemore <admin@secondversion.com>
2012-09-13 16:35:02 -0400

    Updated a few things.

--- admin/delete.php
+++ admin/delete.php
@@ -5,7 +5,7 @@
 * @package   Domain Portfolio Manager
 * @link      http://domain-portfolio.secondversion.com/
 * @version   1.0.1
-* @copyright (C) 2010 - 2011 Eric Sizemore
+* @copyright (C) 2010 - 2012 Eric Sizemore
 * @license   http://domain-portfolio.secondversion.com/docs/license.html GNU Public License
 *
 *            This program is free software: you can redistribute it and/or modify
@@ -20,8 +20,6 @@
 *
 *            You should have received a copy of the GNU General Public License
 *            along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* @file      ./admin/delete.php
 */
 
 /**
@@ -31,18 +29,9 @@
 * As of June 21st, 2010 - Stu Buckingham technically is the copyright holder to DNP up to
 * 1.6.x and he rebranded to DNS Portfolio.
 *
-* The script was originally created by me, Eric Sizemore, and has always been under
-* the GNU GPL. So, with that in mind, I decided to continue development as the rights
-* to 1.7.0 that was in development was not sold. Even with my rights to the code sold
-* for previous versions, with the GPL, I still have the right to fork or continue development.
-*
 * After more than a year of different people coming to me, wanting me to continue working on 
-* Domain Portfolio Manager, I finally decided to pick it back up again.
-*
-* So, this code now is based on the unreleased 1.7.0 I had, and has been re-versioned to start
-* at 1.0.0. It's new name under me is: Domain Portfolio Manager.
-*
-* Enjoy. :)
+* the script, I finally decided to pick it back up again. Since it is under the GNU GPL, I am 
+* able to do so. My continuation of the script will be called Domain Portfolio Manager.
 */
 
 define('IN_DPM', true);
